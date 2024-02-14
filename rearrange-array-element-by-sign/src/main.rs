8cbd12980db6d28ad196ebb7fc842105bb2adc6d
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-02-14 15:23:51 +0545

    Rearrange Array Elements by Sign

---
+++ rearrange-array-element-by-sign/src/main.rs
@@ -0,0 +1,34 @@
+#[allow(unused)]
+pub fn rearrange_array(nums: Vec<i32>) -> Vec<i32> {
+    let mut positive: Vec<i32> = vec![];
+    let mut negative: Vec<i32> = vec![];
+    let mut result: Vec<i32> = vec![];
+    
+    for item in nums.iter() {
+        if *item < 0_i32 {
+            negative.push(*item);
+        } else {
+            positive.push(*item);
+        }
+    }
+    
+    let mut i=0;
+    let length = positive.len();
+    while i< length {
+        result.push(positive[i]);
+        result.push(negative[i]);
+        i += 1;
+    }
+    result
+}
+
+#[allow(unused)]
+fn main() {
+	let nums: Vec<i32> = vec![3,1,-2,-5,2,-4];
+	let res = rearrange_array(nums);
+	println!("{:?}", res);
+	
+	let nums: Vec<i32> = vec![-1,1];
+	let res = rearrange_array(nums);
+	println!("{:?}", res);
+}
