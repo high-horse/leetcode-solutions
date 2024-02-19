84dd051c530cd00b73ffef0cbdd768b92b3841d4
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-02-19 17:20:03 +0545

    power of two

--- power-of-two/src/main.rs
+++ power-of-two/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+
+#[allow(unused)]
+fn main () {
+    let n = 8_i32;
+    // let res = is_power_of_two(n);
+    let res = n & (n-1);
+    println!("is it =>{}", res);
+    let n = 0_i32;
+    let res = is_power_of_two(n);
+    println!("is it =>{}", res);
+    let n = 3_i32;
+    let res = is_power_of_two(n);
+    println!("is it =>{}", res);
+    let n = 2097151_i32;
+    let res = is_power_of_two(n);
+    println!("is it =>{}", res);
 }
+ 
+ 
+#[allow(unused)]
+pub fn is_power_of_two(n: i32) -> bool {
+    n > 0 && n & (n-1) == 0 
+}
+
+
+
+ 
