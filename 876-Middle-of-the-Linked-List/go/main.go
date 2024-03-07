472a4672abf696b31731d3cfac18d865ce7d18a1
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-03-07 12:57:12 +0545

    876. Middle of the Linked List GO impl

---
+++ 876-Middle-of-the-Linked-List/go/main.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+    slow := head
+    fast := head
+
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+
+    return slow
+    
+}
